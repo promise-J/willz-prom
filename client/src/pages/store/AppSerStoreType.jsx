aced5b0745150a0b784da22d7b25f1313657ce22
Promise Johnson <chiemelapromise30@gmail.com>
2025-03-26 20:07:27 +0100

    update

--- client/src/pages/store/AppSerStoreType.jsx
+++ client/src/pages/store/AppSerStoreType.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import ApiSetup from "../../utils/ApiSetup";
+import EmptyData from "../../components/NA/EmptyData";
 
 const AppSerStoreType = () => {
   const { storeType } = useParams();
@@ -27,8 +28,8 @@ const AppSerStoreType = () => {
       <h1 className="text-center text-[25px] md:text-[40px] mt-8 text-blue-900">
         Our <span className="capitalize">{storeType}</span>
       </h1>
-      <div className="grid grid-cols-2 gap-8 p-4">
-        {items.map((item, index) => (
+      <div className={`${items?.length > 0 && "grid"} grid-cols-2 gap-8 p-4`}>
+        {items.length > 0 ? items.map((item, index) => (
           <div key={index} className="md:px-3 shadow-lg rounded-lg py-8 border border-blue-100">
             <h1 className="text-2xl md:text-3xl">{item?.name}</h1>
             <div className="flex gap-4 flex-wrap mt-8">
@@ -42,7 +43,10 @@ const AppSerStoreType = () => {
             <button className="mt-6 w-full border border-blue-900 rounded-lg py-2 font-bold text-blue-900">View</button>
             </Link>
           </div>
-        ))}
+        ))
+        :
+        <EmptyData title={`No ${storeType} Found`} />
+      }
       </div>
     </div>
   );
